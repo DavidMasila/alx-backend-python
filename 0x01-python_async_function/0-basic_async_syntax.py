fbea864db2da37e16a4d3b7a1be506defd9c58eb
MasilaDavid <davidmasila97@gmail.com>
2023-01-09 17:08:42 -0800

    Python Backend

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -12,6 +12,8 @@ import asyncio
 import random
 
 
-async def wait_random(max_delay=10):
-    x = await random.uniform(0, max_delay)
-    return x
+async def wait_random(max_delay: int = 10) -> float:
+    '''Function description above'''
+    random_number = random.uniform(0, max_delay)
+    await asyncio.sleep(random_number)
+    return random_number
