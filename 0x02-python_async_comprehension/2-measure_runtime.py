49ce50ba0cd141f19a9b7a80e0e1abe5b17f1745
MasilaDavid <davidmasila97@gmail.com>
2023-01-10 14:06:47 -0800

    Python Backend

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -8,6 +8,6 @@ async_comp = __import__('1-async_comprehension.py').async_comprehension
 async def measure_runtime() -> float:
     '''Running comprehension multiple times'''
     start_time = time.time()
-    await asyncio.gather(async_comp(), async_comp(), async_comp())
+    await asyncio.gather(async_comp(), async_comp(), async_comp(), async_comp())
     end_time = time.time()
     return end_time - start_time
