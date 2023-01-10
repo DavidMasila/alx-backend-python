1cea2763b75f3aff1d65bab7f057913d282eaadc
MasilaDavid <davidmasila97@gmail.com>
2023-01-10 14:13:16 -0800

    Python Backend

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -1,18 +1,13 @@
-#!/usr/bin/env python3
-
-""" Async Comprehensions """
-
-from asyncio import gather
-from time import time
-
-async_comprehension = __import__('1-async_comprehension').async_comprehension
+#!/usr/bin/env python3 
+'''Run time for four parallel comprehensions'''
+import asyncio
+import time
+async_comp = __import__('1-async_comprehension').async_comprehension
 
 
 async def measure_runtime() -> float:
-    """ Run time for four parallel comprehensions """
-    start = time()
-    tasks = [async_comprehension() for i in range(4)]
-    await gather(*tasks)
-    end = time()
-    return (end - start)
-
+    '''Running comprehension'''
+    start_time = time.time()
+    await asyncio.gather(async_comp(), async_comp(), async_comp())
+    end_time = time.time()
+    return end_time - start_time
