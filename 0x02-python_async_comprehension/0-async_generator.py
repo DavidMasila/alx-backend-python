11e978610160d5b7565d401652c85571ce6e1c80
MasilaDavid <davidmasila97@gmail.com>
2023-01-10 13:25:39 -0800

    Python Backend

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -5,7 +5,7 @@ from random import uniform
 from typing import Generator
 
 
-async def async_generator() -> Generator[float,None, None]:
+async def async_generator() -> Generator[float, None, None]:
     ''' Generator '''
     for _ in range(10):
         yield uniform(0, 10)
