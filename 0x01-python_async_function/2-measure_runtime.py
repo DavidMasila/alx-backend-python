34b3759939f1caf59a988ea1f527737a8e257130
MasilaDavid <davidmasila97@gmail.com>
2023-01-09 18:17:28 -0800

    Python Backend

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -8,11 +8,9 @@ wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
 def measure_time(n: int, max_delay: int) -> float:
-    '''Rem to use asyncio to run the wait_in
-    Because wait_n is a coroutine
-    '''
+    '''Rem to use asyncio to run the wait_in'''
     start = time.time()
     asyncio.run(wait_n(n, max_delay))
     end = time.time()
-    total_time = start - end
+    total_time = end - start
     return total_time / n
