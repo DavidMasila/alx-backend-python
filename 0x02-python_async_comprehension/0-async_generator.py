2627b06446b8d07c3c5ad72bcda5f834d669d040
MasilaDavid <davidmasila97@gmail.com>
2023-01-10 13:25:02 -0800

    Python Backend

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 ''' Async Generator '''
 import asyncio
-import random
+from random import uniform
 from typing import Generator
 
 
 async def async_generator() -> Generator[float,None, None]:
     ''' Generator '''
-    for i in random.uniform(0, 10):
-        yield i
+    for _ in range(10):
+        yield uniform(0, 10)
         await asyncio.sleep(1)
